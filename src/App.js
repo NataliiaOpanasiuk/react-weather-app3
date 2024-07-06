75cd76a9ed8349c738a84ff60c893a236e77cd36
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-07-05 17:57:30 -0700

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Nataliia Opanasiuk and is{" "}
+        <a href="" target="_blank" rel="noreferrer">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a href="" target="_blank" rel="noreferrer">
+          hosted on Netlify
         </a>
-      </header>
+        .
+      </footer>
     </div>
   );
 }
-
-export default App;
