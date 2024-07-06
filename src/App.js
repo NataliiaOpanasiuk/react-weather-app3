870230f0b8e1f245fd5318865cd3f02f52c818aa
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-07-05 19:01:14 -0700

    Created Weather component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Nataliia Opanasiuk and is{" "}
-        <a
-          href="https://github.com/NataliiaOpanasiuk/react-weather-app3"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href=" https://react-app-on.netlify.app/ "
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was created by Nataliia Opanasiuk and is{" "}
+          <a
+            href="https://github.com/NataliiaOpanasiuk/react-weather-app3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href=" https://react-app-on.netlify.app/ "
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
