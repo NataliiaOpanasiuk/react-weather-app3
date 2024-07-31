1bd89e6684a13d1bc295097bad8d24383db8e298
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-07-30 17:33:01 -0700

    Made html changes in Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -18,25 +18,25 @@ export default function Weather() {
           </div>
         </div>
       </form>
+      <h1 className="city">Wexford</h1>
+      <div>Monday, 22 July, 18:08</div>
+      <div>Broken Clouds</div>
       <div className="row">
-        <div className="col-4">
-          <h1>Wexford</h1>
-          <h2>Broken Clouds</h2>
-          <ul>
-            <li>Feels like: 18°C</li>
-            <li>Humidity: 88%</li>
-            <li>Wind: 4 km/h</li>
-          </ul>
-        </div>
-        <div className="col-4">
+        <div className="col-6">
           <img
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
             alt="broken clouds"
           />
+
+          <span className="temperature">18</span>
+          <span className="units"> °C</span>
         </div>
-        <div className="col-4">
-          <h3>Monday, 22 July, 18:08</h3>
-          <h4>18 °C</h4>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 88%</li>
+            <li>Wind: 4 km/h</li>
+          </ul>
         </div>
       </div>
     </div>
