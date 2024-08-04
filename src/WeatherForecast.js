e934b49b351253b7c19b0cdc7edf5e3edaef3021
NataliiaOpanasiuk <nataopanasiuk@gmail.com>
2024-08-03 18:35:14 -0700

    Added weather forecast layout

---
+++ src/WeatherForecast.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <div>
+            <img src={props.icon} />
+          </div>
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">16°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
